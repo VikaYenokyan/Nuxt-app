c67205d71d9d7942d9d7a369188c38f708002b9b
viktoria.e.dterra <v.enokyan@dterra.ru>
2025-03-09 22:00:36 +0300

    init nuxt project

---
+++ pages/contact.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <h1>Contacts</h1>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/"> Главная </NuxtLink></li>
+        <li><NuxtLink to="/contact"> Контакты </NuxtLink></li>
+        <li><NuxtLink to="/users/"> Пользователи </NuxtLink></li>
+      </ul>
+    </nav>
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
